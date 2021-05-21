6530871fb9f74c6795707fc5e4260a21624a3543
Jose Manuel <sanchezmosquerajosemanuel@gmail.com>
2021-05-22 00:17:18 +0200

    Add getFinancialStocksData function

---
+++ TestFinancialReport/TestGetFinancialStocksData.m
@@ -0,0 +1,20 @@
+% TestGetFinancialStocksData
+% 21/05/2021
+
+close all;
+clear;
+clc;
+
+% Load io package to use xlsread()
+pkg load io
+
+addpath('functions');
+
+financialFile = 'data/financialReport20210517.xlsx';
+stockPriceFile = 'data/stockPricesData20210518.xlsx';
+stockInfoFile = 'data/stockInfo20210521.xlsx';
+
+financialData = getFinancialStocksData('financialFile', financialFile, 'stockPriceFile', stockPriceFile, 'stockInfoFile', stockInfoFile);
+
+rmpath('functions');
+clearvars financialFile stockPriceFile stockInfoFile
